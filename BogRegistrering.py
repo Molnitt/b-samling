9bf182e2d74e82bd4547db877007189dc5b187f1
Molnitt <xenia@molnit.com>
2025-02-04 13:31:04 +0100

    efer

---
+++ BogRegistrering.py
@@ -0,0 +1,12 @@
+
+
+}
+
+def RegistrereBog(bog):
+    RegistrerSvar = input("Vil du registrere en bog?")
+    RegistrerSvar = RegistrerSvar.upper()
+    if RegistrerSvar =="JA":
+        BogTitel = input("Hvilken bog vil du registrere?")
+
+    else:
+        print("Intet registreret")
