d9eb083c8b847de5a19312aa463d683275f82dcc
Molnitt <xenia@molnit.com>
2025-03-14 12:21:04 +0100

    Bog registrering

--- BogRegistrering.py
+++ BogRegistrering.py
@@ -1,11 +1,20 @@
+def RegistrereBog(bogRegristrering):
+    RegistrerSvar = input("Vil du registrere en bog? ").upper()
+    if RegistrerSvar == "JA":
+        title = input("Hvilken bog vil du registrere? ")
+        forfatter = input("Hvem er forfatteren af bogen? ")
+        genre = input("Hvilken genre har bogen? ")
+        isbn = input("Hvad er bogens ISBN? ")
 
-
-
-def RegistrereBog(bog):
-    RegistrerSvar = input("Vil du registrere en bog?")
-    RegistrerSvar = RegistrerSvar.upper()
-    if RegistrerSvar =="JA":
-        BogTitel = input("Hvilken bog vil du registrere?")
-
+        ny_bog = Bog(title, forfatter, genre, isbn)
+        bogRegristrering.tilføj_bog(ny_bog)
+        print("Bog er tilføjet til bog samlingen.")
     else:
-        print("Intet registreret")
+        print("Intet registreret.")
+
+def ny_bog():
+
+
+
+
+
