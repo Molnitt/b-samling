caef2ef706aefb87d64e4eb58dec528aa82c1894
Molnitt <xenia@molnit.com>
2025-04-11 12:50:32 +0200

    Bog registrering

--- BogRegistrering.py
+++ BogRegistrering.py
@@ -8,7 +8,10 @@ class Bog:
         self.isbn = isbn
         self.bedømmelse = bedømmelse
 
-class bibliotek:
+    def markerLæst
+
+
+class Bibliotek:
     def __init__(self):
         self.bøger = []
 
@@ -16,18 +19,26 @@ class bibliotek:
         self.bøger.append(bog)
 
 
+bib = Bibliotek()
 
-def RegistrereBog(bogRegristrering):
+
+def registrereBog():
     RegistrerSvar = input("Vil du registrere en bog? ").upper()
     if RegistrerSvar == "JA":
         title = input("Hvilken bog vil du registrere? ")
         forfatter = input("Hvem er forfatteren af bogen? ")
         genre = input("Hvilken genre har bogen? ")
         isbn = input("Hvad er bogens ISBN? ")
+        ny_bog = Bog(title, forfatter, genre, isbn)
+        bib.tilføj(ny_bog)
+
+registrereBog()
+print(bib.bøger)
 
 
-        Bog(title, forfatter, genre, isbn)
+#def bedømmelseSystem():
+   # BedømmelseSvar = input("Vi du bedømme en tidligere regristreret bog?").upper()
+   # if BedømmelseSvar == "JA":
+       # bogvalg = input("Hvilken bog?")
 
-        print("Bog er tilføjet til bog samlingen.")
-    else:
-        print("Intet registreret.")
+
